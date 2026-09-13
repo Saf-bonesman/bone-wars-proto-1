6cb06513402cefaae709659de99f66fbe25fc6b4
Saff Euan B <saffi.bannister@gmail.com>
2026-09-13 19:15:05 +1000

    player offset

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,31 +1,26 @@
 extends Node2D
 
 ## generates and draws a board state
-@export var board_width : int = 8
-@export var board_height : int = 6
 @export var terrain : TileMapLayer
-const tileSize = 16
-var board : Dictionary = {}
+var tileSize
 const terrainDict : Dictionary = {
 	"wastes" = 0,
 	"bones" = 1
 }
 
-func _ready() -> void:
-	_init_board()
-	redraw_board()
-
-func _init_board() -> void:
+func _init_board(board_width, board_height) -> Dictionary:
+	var board : Dictionary = {}
 	for x in range(board_width):
 		for y in range(board_height):
 			var Cell = Tile.new()
 			var Location = Marker2D.new()
 			Location.position.y = y * tileSize
 			Location.position.x = x * tileSize
-			board[Vector2i (x+1,y)] = Cell
+			board[Vector2i (x,y)] = Cell
+	return board
 
-## actually draw the tiles
-func redraw_board() -> void:
+## actually draw the tile
+func redraw_board(board : Dictionary) -> void:
 	for cell in board:
 		var terrainIndex = terrainDict.get(board.get(cell).TileType)
 		terrain.set_cell(cell, terrainIndex, Vector2i(0,0), 0)
--- scripts/map.gd
+++ scripts/map.gd
@@ -9,3 +9,18 @@ extends Node2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 	#pass
+
+@export var board_width : int = 8
+@export var board_height : int = 6
+const tile_height = 16
+@onready var board_node : Node2D = $Board
+var board_dict : Dictionary
+@onready var Player : Node2D = $Player
+
+func _ready() -> void:
+	board_node.tileSize = tile_height
+	board_dict = board_node._init_board(board_width, board_height)
+	board_node.redraw_board(board_dict)
+	Player.tile_height = tile_height
+	Player.w = board_width - 1
+	Player.h = board_height - 1
