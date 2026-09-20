b8353244d8ae0a2cc02d737982f344311d702b36
Saf <ssafeer15@gmail.com>
2026-09-20 16:00:29 +1000

    connected menu to game controller

--- scenes/player_menu.gd
+++ scenes/player_menu.gd
@@ -4,26 +4,27 @@ var menu_buttons : Array[Button]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_focus()
+	_set_focus()
 	
 func setup(acts_menu : Array[String]) -> void:
 	for act in acts_menu:
-		self.add_button(act)
-	self.add_menu(menu_buttons)
+		self._add_button(act)
+	self._add_menu(menu_buttons)
 	
 #func add_action(text: String, action: Callable) -> void:
 	#var button := add_button(Button.new(), text) as Button
 
-func add_button(text: String) -> Button:
+func _add_button(text: String) -> Button:
 	var button := Button.new() as Button
 	button.text = text
+	button.pressed.connect(_button_pressed.bind(button))
 	button.alignment = HORIZONTAL_ALIGNMENT_CENTER
 	button.custom_minimum_size = Vector2i(64, 16)
 	button.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	menu_buttons.append(button)
 	return button
 
-func add_menu(buttons : Array[Button]) -> void:
+func _add_menu(buttons : Array[Button]) -> void:
 	match menu_buttons.size():
 		2:
 			self.size = Vector2i(64, 32)
@@ -36,8 +37,26 @@ func add_menu(buttons : Array[Button]) -> void:
 	for butt in buttons:
 		%VBoxContainer.add_child(butt)
 	
-func set_focus() -> void:
+func _set_focus() -> void:
 	if not menu_buttons.is_empty():
 		var first: Button = menu_buttons.front()
 		if first:
 			first.grab_focus()
+
+signal broadcast_menu_action
+
+func _input(_event) -> void:
+	if Input.is_action_pressed("action_b"):
+		broadcast_menu_action.emit("exit_menu")
+
+func _button_pressed(act_button : Button):
+	match act_button.text:
+		"Pass":
+			#disable camp
+			broadcast_menu_action.emit("exit_menu")
+		"Back":
+			broadcast_menu_action.emit("exit_menu")
+		"Sabotage":
+			broadcast_menu_action.emit("sabotage")
+		"Dig":
+			broadcast_menu_action.emit("dig")
--- scripts/player.gd
+++ scripts/player.gd
@@ -45,8 +45,8 @@ func _input(_event) -> void:
 			move_timer.set_paused(true)
 			cursor_sprite.play("default", .2, false)
 			# inputs while menuing are handled by menu
-			if Input.is_action_pressed("action_b"):
-				broadcast_action.emit("exit_menu")
+			#if Input.is_action_pressed("action_b"):
+				#broadcast_action.emit("exit_menu")
 		player_state.ACTION_SABOTAGE:
 			if Input.is_action_pressed("action_a"):
 				broadcast_action.emit("sabotage")
@@ -69,19 +69,29 @@ func _on_move_timer_timeout() -> void:
 func _handle_move() -> void:
 	if Input.is_action_pressed("action_left"):
 		curr_pos.x -= 1
-		# print("moved l " + str(curr_pos.x) + ", " + str(curr_pos.y))
 	if Input.is_action_pressed("action_right"):
 		curr_pos.x += 1
-		# print("moved r " + str(curr_pos.x) + ", " + str(curr_pos.y))
 	if Input.is_action_pressed("action_up"):
 		curr_pos.y -= 1
-		# print("moved up " + str(curr_pos.x) + ", " + str(curr_pos.y))
 	if Input.is_action_pressed("action_down"):
 		curr_pos.y += 1
-		# print("moved down " + str(curr_pos.x) + ", " + str(curr_pos.y))
 	limit_pos()
 	position = Vector2i(
 		curr_pos.x * tile_height + tile_height/2, 
 		curr_pos.y * tile_height + tile_height/2)
+
+#func set_state(text_act : String) -> void:
+	#match text_act:
+		#"Sabotage":
+			#current_state = player_state.ACTION_SABOTAGE
+		#"Dig":
+			#current_state = player_state.ACTION_DIG
+		#"Pass":
+			##disable camp?
+			#broadcast_action.emit("exit_menu")
+			#current_state = player_state.SELECT_CAMP_FOR_ACTION
+		#"Back":
+			#broadcast_action.emit("exit_menu")
+			#current_state = player_state.SELECT_CAMP_FOR_ACTION
 
 signal broadcast_action
