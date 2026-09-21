648661df1a3d22fa1a643832d285a5c4ca74e941
Saf <ssafeer15@gmail.com>
2026-09-21 20:31:32 +1000

    splash screen

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var HUD : Control = $HUD
 @onready var Map : Node2D = $Map
 @onready var Enemy : Node = $EnemyAI
+@onready var Splash : Sprite2D = $Splash
 var score_array : Array[int] = [0, 0]
 var current_player_turn : int = 0
 @export var player_count : int = 1
@@ -39,6 +40,9 @@ var game_state : String
 
 func _ready() -> void:
 	_restart_game()
+	Splash.set_visible(true)
+	await get_tree().create_timer(3.0).timeout
+	Splash.set_visible(false)
 	Map.Player.broadcast_action.connect(_on_broadcast_action)
 	Map.Camps.refresh_done.connect(_on_refresh)
 
